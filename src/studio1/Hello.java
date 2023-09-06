655979a3da6fe9a022b58fe9363e9f2c4484bd41
kuan <kuan@localhost>
2023-09-06 14:21:06 -0500

    lab1

--- src/studio1/Hello.java
+++ src/studio1/Hello.java
@@ -12,6 +12,9 @@ public class Hello {
 		
 		System.out.println("Hello and welcome to CSE 131!");
 		System.out.println("You seem to be feeling " + mood);
+		
+		String 
+		System.out.println("How are you feeling today?");
 	}
 
 }
